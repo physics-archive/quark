3286ed059b3a0e5f358f53fa100595ee2dfd41ad
envy <envy@crabragoon99.org>
2024-06-19 01:40:43 -0400

    starting

--- firmware.c
+++ firmware.c
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-#include <lib/stm32/f4/rcc.o>
-
-static void rcc_setup(void) {
-    rcc_clock_setup_pl1();
-}
-
-=======
+//Firmware 2024.06.18
 #include <include/libopencm3/stm32/f4/rcc.h>
 #include <include/libopencm3/stm32/f4/gpio.h>
 
-//Firmware 2024.06.18
->>>>>>> c3b7bb0fdbacdb4b772946c47f424b8b3eafd14c
+static void rcc_setup(void) {
+    rcc_clock_setup_pll();
+}
 
 int main(void) {
 
