04ab7dbb69f361459e9d24b2e52f518819a75c98
envy <envy@crabragoon99.org>
2024-06-20 21:09:36 -0400

    windows dev environment

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 To get started with c libraries: 
 - Git submodule init && git submodule update
+- Install Arm-GNU-Toolchain on Windows or Unix
+- Make sure c_cpp_properties.json has the correct compilterPath specified!
+	- Different for Windows, MacOS and Linux
--- firmware.c
+++ firmware.c
@@ -1,15 +1,15 @@
 //Firmware 2024.06.18
-#include <include/libopencm3/stm32/f4/rcc.h>
-#include <include/libopencm3/stm32/f4/gpio.h>
+#include <libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/stm32/f4/gpio.h>
 
 static void rcc_setup(void) {
-    rcc_clock_setup_pll(rcc_hsi_configs[]);
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
 }
 
 int main(void) {
 
     while (1) {
-
+        
     }
 
     return 0;
